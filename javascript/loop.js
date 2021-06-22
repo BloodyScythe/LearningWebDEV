349c4cf1e93f629dc43a830be2967688219313a7
Dhruv kaith <kaithdhruv@gmail.com>
2021-06-22 17:57:35 +0530

    arrow function

--- javascript/loop.js
+++ javascript/loop.js
@@ -62,7 +62,7 @@ setMessage("hello Mom!",displayDOM); */
 
 // nested function\
 
-function userLogin(){
+/* function userLogin(){
 
     username = "Dhruv";
     inbox=0;
@@ -77,5 +77,205 @@ function userLogin(){
     userID();
 }
 userLogin();
+ */
+
+//closure method
+
+/* function fontSize(size){
+
+    return function(){                      //annoyomus function to retain value of any variale that would have been used in it
+
+        document.body.style.fontSize = size + "px";
+    }
+}
+
+function fontStyle(style){
+
+    return function(){ 
+
+        document.body.style.fontFamily = style;
+    }
+}
 
 
+let size20 = fontSize(20);
+let size30 = fontSize(30);
+
+size20();
+size30(); */
+
+//arrow function
+
+/* function hello(){
+
+    return "hello world";
+} */
+
+/* hello = () => "hello world";
+
+console.log(hello()); */
+
+//multi para 
+
+/* let x = "Dhruv";
+let y = "kaith";
+
+hello = (x,y) => x+" "+y+" Hello"; //you don't need () for single argument 
+
+console.log(hello(x,y)); */
+
+// ******* index.js *******
+
+ 
+
+// arrow function =>
+
+// shortcut for a function expression
+
+/*  
+
+let hello;
+
+let x = "Yo";
+
+let y = "bro!";
+
+
+
+// *********************
+
+// function expression
+
+// *********************
+
+
+
+hello = function() {
+
+  return "Hello World!";
+
+}
+
+console.log(hello());
+
+
+
+// *********************
+
+// arrow function
+
+// *********************
+
+
+
+hello = () => "Hello World!";
+
+console.log(hello());
+
+ 
+
+// *********************
+
+// arrow function w/ 1 argument
+
+// *********************
+
+
+
+hello = x => x + "World!";
+
+console.log(hello(x));
+
+ 
+
+// *********************
+
+// arrow function w/ multiple arguments
+
+// *********************
+
+
+
+hello = (x,y) => x + y;
+
+console.log(hello(x, y));
+
+ 
+
+// *********************
+
+// arrow function w/ multiple lines
+
+// *********************
+
+
+
+hello = (x,y) => {
+
+    console.log(x);
+
+    console.log(y);
+
+};
+
+hello(x,y);
+
+ 
+
+// *********************
+
+// MAP
+
+// *********************
+
+
+
+let storeUSD = [5, 6, 7, 8, 9];
+
+let storeEUR;
+
+ 
+
+storeEUR = storeUSD.map(value => value *= 0.84);
+
+console.log(storeEUR);
+
+ 
+
+// *********************
+
+// FILTER
+
+// *********************
+
+
+
+let students = [16, 17, 18, 19, 20];
+
+let adultStudents;
+
+ 
+
+adultStudents = students.filter(age => age >= 18);
+
+console.log(adultStudents);
+
+ 
+
+// *********************
+
+// REDUCE
+
+// *********************
+
+
+
+let letters = ["H", "E", "L", "P"];
+
+let word;
+
+ 
+
+word = letters.reduce((total, nextLetter) => total + nextLetter);
+
+console.log(word); */
