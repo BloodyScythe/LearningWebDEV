0741ab45447798fe37cca5d2e1cae513f45b4605
Dhruv kaith <kaithdhruv@gmail.com>
2021-06-27 11:45:52 +0530

    basic structure

--- javascript/arrayfunction.js
+++ javascript/arrayfunction.js
@@ -2,6 +2,7 @@
 
 let vals = [4,5,8,9,1,5];
 console.log(vals);
+//with arrrow
 vals = vals.map( x => x * 2);
 console.log(vals);
 
@@ -11,3 +12,42 @@ console.log(vals);
 let fillArr = [4,7,2,7];
 fillArr.fill(Math.random());
 console.log(fillArr);
+
+//***************************for each()************************///
+
+let total = 0;
+let cart = [5,6,7,8];
+//without arrow
+function cartTotal(element){
+    total+= element;
+
+}
+cart.forEach(cartTotal);
+console.log(total);
+
+//with arrow
+
+
+//***************filter()****************** */
+
+let students = [16,17,18,19,20];
+
+function checkStudentAge(age) {
+    if(age>=18)
+    return age
+}
+
+let adultStudent = students.filter(checkStudentAge);
+console.log(adultStudent);
+
+//*********  reduce() *********************/
+
+let word = ['H','E','L','P'];
+//without arrow
+function combine(accumalator, nexword){
+
+    return accumalator += nexword;
+}
+
+let fullword = word.reduce(combine);
+console.log(fullword);
